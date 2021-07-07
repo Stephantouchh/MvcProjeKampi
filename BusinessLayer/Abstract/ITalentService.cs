bfa92205c289827f4b453bbf1f043335c1430cda
Stephantouchh <81231717+Stephantouchh@users.noreply.github.com>
2021-07-07 12:56:30 +0300

    güncelleme

---
+++ BusinessLayer/Abstract/ITalentService.cs
@@ -0,0 +1,18 @@
+﻿using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Abstract
+{
+    public interface ITalentService
+    {
+        List<Talent> GetTalents();
+        void Insert(Talent talent);
+        void Update(Talent talent);
+        void Delete(Talent talent);
+        Talent GetById(int id);
+    }
+}
