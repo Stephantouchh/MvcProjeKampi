b2e90960a716928e6cd81dcbb5b587eaee35bd69
Stephantouchh <81231717+Stephantouchh@users.noreply.github.com>
2021-07-07 12:57:42 +0300

    güncelleme

--- BusinessLayer/Concrete/ImageFileManager.cs
+++ BusinessLayer/Concrete/ImageFileManager.cs
@@ -22,5 +22,10 @@ namespace BusinessLayer.Concrete
         {
             return _imagefileDal.List();
         }
+
+        public void ImageAdd(ImageFile ımageFile)
+        {
+            _imagefileDal.Insert(ımageFile);
+        }
     }
 }
